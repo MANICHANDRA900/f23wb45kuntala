c54f16ec187d58300fa10b413a64698b60202ca8
MANICHANDRA900 <143015834+MANICHANDRA900@users.noreply.github.com>
2023-11-01 20:24:43 -0500

    Working with query strings in the pug engine

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var equipmentRouter = require('./routes/equipment');
+var boardRouter = require('./routes/board');
 
 var app = express();
 
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/equipment', equipmentRouter);
+app.use('/board', boardRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
